fa65c49cb2db82082149a7eb81ef94c0efacd289
Maire Chew <mairechew@gmail.com>
2017-07-24 15:01:22 -0600

    completed stoplight exercises

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+var stop = document.getElementById('stopButton')
+var stopBtn = document.getElementById('stopLight')
+stop.addEventListener("click", function() {
+    if (stopBtn.style["background-color"] === 'red') {
+      stopBtn.style["background-color"] = 'black'
+    } else {
+      stopBtn.style["background-color"] = 'red'
+    };
+  })
+
+stop.addEventListener("mouseenter", function() {
+  console.log("Entered " + stop.textContent +  " button")
+})
+
+var slow = document.getElementById('slowButton')
+var slowBtn = document.getElementById('slowLight')
+slow.addEventListener("click", function () {
+  if (slowBtn.style["background-color"] === "orange") {
+    slowBtn.style["background-color"] = "black"
+  } else {
+    slowBtn.style["background-color"] = "orange"
+  };
+})
+
+slow.addEventListener("mouseenter", function() {
+  console.log("Entered " + slow.textContent +  " button")
+})
+
+var go = document.getElementById('goButton')
+var goBtn = document.getElementById('goLight')
+go.addEventListener("click", function () {
+  if (goBtn.style["background-color"] === "green") {
+    goBtn.style["background-color"] = "black"
+  } else {
+    goBtn.style["background-color"] = "green"
+  };
+})
+
+go.addEventListener("mouseenter", function() {
+  console.log("Entered " + go.textContent +  " button")
+})
+
+
+
+
+
 })();
